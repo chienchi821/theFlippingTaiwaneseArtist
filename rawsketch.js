aebed9d7655266594c49dd53eff6440e17410773
chienchi821 <73259376+chienchi821@users.noreply.github.com>
2020-11-27 21:54:23 +0800

    1127

--- rawsketch.js
+++ rawsketch.js
@@ -1,16 +1,14 @@
 var sentence = "舔共藝人 賣國求榮 賤畜 習近平的形狀 破腦麻仔 陪睡 噓 噁 討厭 胖 蔣開穴氣到棺材板跳起來 槍斃 沒存在感 米田共 垃圾 爛咖 煩";
 
-for (let i = 0; i < 10; i++) {
+for (let i = 0; i < 15; i++) {
   $("#message").append(
-    $('<div/>', { 
-      class: 'random-sentence', 
-      html: sentence 
+    $('<div/>', {
+      class: 'random-sentence',
+      html: sentence
     }).css({
       fontSize: random(15, 50)+'px',
       textOrientation: random(0,2) == 1?'upright': random(0,1) == 1?'sideways':'sideways-right',
-
-    })
-  );
+    }));
 }
 
 $("#message").on('click', '.random-sentence', function() {
